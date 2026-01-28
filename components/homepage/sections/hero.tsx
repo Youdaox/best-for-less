fc6dfe9d344dfb0fd2d01dabb9dfb3831ea7cef8
Youdaox <youdao005@gmail.com>
2026-01-28 23:55:03 +1300

    created heroTextBlock compoment to extract text into own component

--- components/homepage/sections/hero.tsx
+++ components/homepage/sections/hero.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import QuoteForm from "../quoteForm";
 
+const HeroTextBlock = () => {
+  return (
+    <div className="max-w-3xl">
+      <h1 className="text-white text-4xl sm:text-5xl font-bold px-4">
+        Quality Service At The Best Price
+      </h1>
+      <p className="text-white text-md sm:text-lg mt-4 px-4">
+        Best for Less mover is a Man with a Van business with over 20 years of professionalism. We are equipped with a team of experienced workers and a 20 cubic metre truck ready to handle your move. We give our customers the most competitive prices in the market for the smoothest experience. Choose us for your next move.
+      </p>
+    </div>
+  );
+}
+
 const Hero = () => {
   return (
-    <div>
-      <div className="absolute inset-0 -z-10">
+    <div className="mb-32">
+      <div>
         <Image
           src="/hero.png"
           alt="Hero Image"
@@ -16,9 +29,7 @@ const Hero = () => {
       <div className="relative w-full h-196 rounded-md shadow-md">
         <div className="absolute inset-0 bg-black/40" />
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-4xl sm:text-5xl font-bold text-center px-4">
-            Quality Service At The Best Price
-          </h1>
+          <HeroTextBlock />
           <QuoteForm />
         </div>
       </div>
