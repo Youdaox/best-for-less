387e5d2143d176186f5d5de07b243608746c4df4
Youdaox <youdao005@gmail.com>
2026-02-09 22:07:40 +1300

    created testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/homepage/sections/hero";
 import Process from "@/components/homepage/sections/process";
 import Services from "@/components/homepage/sections/services";
 import Subheading from "@/components/homepage/sections/subheading";
+import Testimonials from "@/components/homepage/sections/testimonials";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Subheading />
       <div className="container mx-auto flex justify-center px-12 flex-col flex-1">
         <Services />
+        <Testimonials />
         <Faq />
       </div>
     </div>
---
+++ components/homepage/sections/testimonials.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
+const TestimonialCard= ({ text, author }: { text: string; author: string }) => {
+  return (
+    <Card className="relative mx-auto max-w-sm py-6 hover:shadow-lg transition-shadow duration-300s hover:opacity-85 ">
+      <CardContent className="text-lg max-w-sm space-y-4">
+        <CardDescription>{text}</CardDescription>
+        <Separator />
+      </CardContent>
+      <CardFooter className="flex flex-col items-start">
+        <h2 className="text-lg font-semibold">{author}</h2>
+        <div className="flex justify-between w-full mt-2">
+          <h3 className="text-sm text-slate-500">Google</h3>
+          <h3 className="text-sm text-slate-500">⭐⭐⭐⭐⭐</h3>  
+        </div>
+       </CardFooter>
+    </Card>
+  )
+}
+
+const Testimonials = () => {
+  return (
+    <section className="my-50">
+      <div className="mx-auto max-w-3xl text-center mb-12 space-y-4">
+        <h2 className="text-5xl font-bold">What People are Saying about Best for Less Mover</h2>
+        <p>Our customers are our top priority, and we're dedicated to providing the best possible service at the most competitive prices. Here's what a few of our satisfied customers have to say about us.</p>
+      </div>
+      <div className="flex flex-wrap justify-center gap-6">
+        <TestimonialCard text="Best for Less has saved me so much money on my shopping! I can't recommend it enough." author="Sarah K." />
+        <TestimonialCard text="I love how easy it is to find the best deals with Best for Less. It's my go-to shopping companion." author="John D." />
+        <TestimonialCard text="Thanks to Best for Less, I've been able to stick to my budget and still get everything I need." author="Emily R." />
+      </div>
+    </section>
+  )
+}
+
+export default Testimonials
